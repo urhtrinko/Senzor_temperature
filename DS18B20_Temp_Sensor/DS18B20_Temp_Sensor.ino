f0e0394cf7151370d004e0f1a00937004aa0f9ef
urh_trinko <urh.trinko@gmail.com>
2024-09-10 11:13:52 +0200

    Update DS18B20_Temp_Sensor.ino
    Sprememba inputa iz pin 2 na pin 7

--- DS18B20_Temp_Sensor/DS18B20_Temp_Sensor.ino
+++ DS18B20_Temp_Sensor/DS18B20_Temp_Sensor.ino
@@ -7,8 +7,8 @@
 #include <DallasTemperature.h>
 #define STOP_COMMAND "STOP"  // Command to stop execution
 
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+// Data wire is plugged into pin 7 on the Arduino
+#define ONE_WIRE_BUS 7
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
